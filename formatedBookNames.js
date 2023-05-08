1d93ff6360873d6bc02ba8dcf45ecae0c7ebdbdf
Anna Escolaro <annaescolaro@hotmail.com>
2023-05-08 20:18:36 -0300

    requisito 1

--- formatedBookNames.js
+++ formatedBookNames.js
@@ -1,7 +1,10 @@
-// const { books } = require('./data/library');
+const { books } = require('./data/library');
 
-const formatedBookNames = () => {
-  // escreva seu código aqui
-};
+const formatedBookNames = () => books.map((book) => {
+  const bookName = book.name;
+  const bookGenre = book.genre;
+  const bookAuthor = book.author.name;
+  return `${bookName} - ${bookGenre} - ${bookAuthor}`;
+});
 
 module.exports = { formatedBookNames };
