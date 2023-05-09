ab5243516d67013bdca1f3f6e22e9be61e290d82
Anna Escolaro <annaescolaro@hotmail.com>
2023-05-08 21:19:01 -0300

    req 3

--- nameAndAge.js
+++ nameAndAge.js
@@ -1,7 +1,15 @@
-// const { books } = require('./data/library');
+const { books } = require('./data/library');
 
-const nameAndAge = () => {
-  // escreva seu código aqui
-};
+const nameAndAge = () => books.map((book) => {
+  const authorName = book.author.name;
+  const authorAge = (book.releaseYear - book.author.birthYear);
+  const nameAndAgeObj = {
+    author: authorName,
+    age: authorAge,
+  };
+  return nameAndAgeObj;
+}).sort((a, b) => a.age - b.age);
+
+console.log(nameAndAge());
 
 module.exports = { nameAndAge };
